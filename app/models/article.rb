91f565226cae2c26d1f1c289cca86ea16cde47b6
Jeremy Gradisher <jeremygradisher@gmail.com>
2016-01-12 20:12:44 +0000

    Fix create, add flash and error messages, create show, fix validation screw up

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: { minumum: 3, maximum: 50 }
-    validates :description, presence: true, length: { minumum: 10, maximum: 1500 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 1500 }
 end
